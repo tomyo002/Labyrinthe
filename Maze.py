a5011b333adf4f240f5df061e01a7b295f6492a2
toml daret <daret.tom@gmail.com>
2023-03-03 13:20:18 +0100

    rajout du paramètre empty du constructeur maze

---
+++ Maze.py
@@ -0,0 +1,87 @@
+from random import *
+
+class Maze:
+    """
+    Classe Labyrinthe
+    Représentation sous forme de graphe non-orienté
+    dont chaque sommet est une cellule (un tuple (l,c))
+    et dont la structure est représentée par un dictionnaire
+      - clés : sommets
+      - valeurs : ensemble des sommets voisins accessibles
+    """
+    def __init__(self, height, width, empty):
+        """
+        Constructeur d'un labyrinthe de height cellules de haut 
+        et de width cellules de large 
+        Les voisinages sont initialisés à des ensembles vides
+        Remarque : dans le labyrinthe créé, chaque cellule est complètement emmurée
+        """
+        self.height    = height
+        self.width     = width
+      
+
+        if  empty:
+            self.neighbors = {}
+            for i in range(height):
+                for j in range(width):
+                    ensemble = set()
+                    if i-1 >=0:
+                        ensemble.add((i-1,j))
+                    if i+1 < height:
+                        ensemble.add((i+1,j))
+                    if j-1 >=0:
+                        ensemble.add((i,j-1))
+                    if j+1 < width:
+                        ensemble.add((i,j+1))
+                    self.neighbors[i,j]= ensemble
+        else:
+            self.neighbors = {(i,j): set() for i in range(height) for j in range (width)}
+
+                        
+
+
+    def info(self):
+        """
+        Affichage des attributs d'un objet 'Maze' (fonction utile pour deboguer)
+        Retour:
+            chaîne (string): description textuelle des attributs de l'objet
+        """
+        txt = f"{self.height} x {self.width}\n"
+        txt += str(self.neighbors)
+        return txt
+
+    def __str__(self):
+        """
+        Représentation textuelle d'un objet Maze (en utilisant des caractères ascii)
+        Retour:
+             chaîne (str) : chaîne de caractères représentant le labyrinthe
+        """
+        txt = ""
+        # Première ligne
+        txt += "┏"
+        for j in range(self.width-1):
+            txt += "━━━┳"
+        txt += "━━━┓\n"
+        txt += "┃"
+        for j in range(self.width-1):
+            txt += "   ┃" if (0,j+1) not in self.neighbors[(0,j)] else "    "
+        txt += "   ┃\n"
+        # Lignes normales
+        for i in range(self.height-1):
+            txt += "┣"
+            for j in range(self.width-1):
+                txt += "━━━╋" if (i+1,j) not in self.neighbors[(i,j)] else "   ╋"
+            txt += "━━━┫\n" if (i+1,self.width-1) not in self.neighbors[(i,self.width-1)] else "   ┫\n"
+            txt += "┃"
+            for j in range(self.width):
+                txt += "   ┃" if (i+1,j+1) not in self.neighbors[(i+1,j)] else "    "
+            txt += "\n"
+        # Bas du tableau
+        txt += "┗"
+        for i in range(self.width-1):
+            txt += "━━━┻"
+        txt += "━━━┛\n"
+
+        return txt
+    
+   
---
+++ TestMaze.py
@@ -0,0 +1,5 @@
+from Maze import Maze
+
+laby = Maze(4, 4, empty=True)
+print(laby)
+
