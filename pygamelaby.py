78d4633ae0ed6ab3ef927f98f7f04c0988c8aa1e
tom daret <daret.tom@gmail.com>
2023-03-07 19:57:33 +0100

    ajout de malus random laby

--- pygamelaby.py
+++ pygamelaby.py
@@ -36,6 +36,7 @@ def constuct_laby(screen,laby,x,y):
 
 
 pygame.init()
+pygame.font.init()
 
 size = width, height = 700, 700
 
@@ -50,7 +51,7 @@ perso = pygame.transform.scale(pygame.image.load("imgPygame/tickeyBoy.png"),(40,
 door = pygame.transform.scale(pygame.image.load("imgPygame/door.png"),(40,40))
 levelName = pygame.transform.scale(pygame.image.load("imgPygame/levelName.png"),(40,40))
 pierreTp =  pygame.transform.scale(pygame.image.load("imgPygame/pierreTp.png"),(40,40))
-
+malusSwitch= pygame.transform.scale(pygame.image.load("imgPygame/malusswitch.png"),(40,40))
 
 level = True
 nblevel=1
@@ -72,14 +73,38 @@ while True:
     pygame.display.update()
     coor_tp1=(-1,-1)
     coor_tp2=(-1,-1)
+    coorMS = (-1,-1)
+    isMs = False
+    listeMs =[]
+    
+    smallfont = pygame.font.SysFont('nom',40)
+    text=smallfont.render(str(nblevel), True , pygame.Color('white'))
+    screen.blit(text ,(660,60))
 
     if nblevel >= 5:
         valTp = randint(1,2)
         if valTp ==1:
             coor_tp1=(randint(0,x-1),randint(0,y-1))
             coor_tp2=(randint(0,x-1),randint(0,y-1))
+            while coor_tp1 ==(x-1,y-1): 
+                coor_tp2=(randint(0,x-1),randint(0,y-1))
+            while coor_tp2 ==(x-1,y-1): 
+                coor_tp1=(randint(0,x-1),randint(0,y-1))
             screen.blit(pierreTp,(coor_tp1[1]*50+15,coor_tp1[0]*50+15))
             screen.blit(pierreTp,(coor_tp2[1]*50+15,coor_tp2[0]*50+15))
+
+    if nblevel >=3:
+        valSwitch =randint(1,2)
+        if valSwitch ==1:
+            nbSwitch = randint(3,5)
+            for i in range(nbSwitch):
+                coorMS = (randint(0,x-1),randint(0,y-1))
+                while coorMS == (x-1,y-1):
+                    coorMS = (randint(0,x-1),randint(0,y-1))
+                screen.blit(malusSwitch,(coorMS[1]*50+15,coorMS[0]*50+15))
+                listeMs.append(coorMS)
+            
+            isMs = True
     
     while level:
         for event in pygame.event.get():
@@ -142,6 +167,25 @@ while True:
                     screen.blit(perso,pos_perso)
                     screen.blit(pierreTp,(coor_tp2[1]*50+15,coor_tp2[0]*50+15))
                     pygame.display.update()
+
+                if (coorPerso) in listeMs and isMs:
+                    laby=Maze.gen_wilson(x,y)
+                    constuct_laby(screen,laby,x,y)
+                    screen.blit(perso,pos_perso)
+                    screen.blit(door,((x-1)*50+15,(y-1)*50+15))
+                    screen.blit(pierreTp,(coor_tp1[1]*50+15,coor_tp1[0]*50+15))   
+                    screen.blit(pierreTp,(coor_tp2[1]*50+15,coor_tp2[0]*50+15))
+                    screen.blit(levelName,(650,10))
+                    listeMs.remove(coorPerso)
+                    screen.blit(text ,(660,60))
+                    if len(listeMs)==0:
+                        isMs = False
+                    else:
+                        for i in listeMs:
+                            screen.blit(malusSwitch,(i[1]*50+15,i[0]*50+15))
+                    pygame.display.update()
+                    
+
     
     level = True
     if x >= 7:
